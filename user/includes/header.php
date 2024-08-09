38804cd06650f1b1cb61610bb8dfeffc879ae8c9
Rifkah Syam <rfkhikaa@gmail.com>
2024-08-09 19:29:29 +0700

    Add files via upload

---
+++ user/includes/header.php
@@ -0,0 +1,32 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="index.php">
+            <img src="assets/img/logo.png" alt="Scientia Square Park" style="width: 50px; height: 50px;">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Beranda</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php#welcome">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php#gallery">Galeri</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="facilities.php">Fasilitas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="tour-packages.php">Paket Wisata</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="reservations.php">Daftar Reservasi</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
